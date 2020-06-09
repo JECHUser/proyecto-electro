2d6b8854043310add30c584e14306f315705a0ad
JECHUser <66657576+JECHUser@users.noreply.github.com>
2020-06-08 21:43:02 -0600

    Update cil1_graf.m

--- cilindricas/cil1_graf.m
+++ cilindricas/cil1_graf.m
@@ -1,30 +1,25 @@
 clc
-
+% definicion de variables
 theta = linspace(0,2*pi);
 r = 10;
 s = linspace(10,30,5);
 e_0 = 8.84*10^-12;
 E_0 = 5;
 
+% creacion de grilla de graficacion
 [theta, s] = meshgrid(theta, s);
 
+% potencial en el exterior del cilindro
 z = -E_0.*s.*cos(theta).*(((r.^2)./(s.^2))-1);
+
+% conversion polar a cartesiana
 [x,y,z] = pol2cart(theta, s, z);
+
+% creacion de superficie
 surf(x,y,z)
 colorbar
+
+% ejes
 xlabel("x")
 ylabel("y")
 zlabel("V")
-
-% subplot(1,2,2)
-% x = s.*cos(theta);
-% y = s.*sin(theta);
-% z = -E_0.*s.*cos(theta).*(((r.^2)./(s.^2))-1);
-% z = -E_0.*((x.^2+y.^2).^(1/2)).*cos(atan(y./x)).*(((r.^2)./(((x.^2+y.^2).^(1/2)).^2))-1);
-% 
-% [U,V] = gradient(-z);
-% quiver3(x,y,z,U,V,z)
-% xlabel("x")
-% ylabel("y")
-% zlabel("V")
-
